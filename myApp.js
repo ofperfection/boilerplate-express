d03c7e5a0a60d5c6479c6f08e54aa27d5d388d65
Sean <sean@saibotdigital.com>
2020-10-27 05:46:07 +0000

    Couple more

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 // --> 7)  Mount the Logger middleware here
 
 
@@ -40,18 +40,20 @@ app.get('/now', (req,res, next)=>{
 }
 );
 
-/** 9)  Get input from client - Route parameters */
+/** 9)  Get input from client - Route parameters 
 app.get('/:word/echo', (req,res) => {
   res.json({echo: req.params.word});
 });
 
-/** 10) Get input from client - Query parameters */
+/** 10) Get input from client - Query parameters 
 // /name?first=<firstname>&last=<lastname>
-
+app.get('/name', (req,res)=>{
+  res.json({name: `${req.query.first} ${req.query.last}`});
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 /** 12) Get data form POST  */
 
